0c3e2d39cc32829c2aecb257dd07592c0da03a13
Dark_Shadow04 <32217440+DarkShadow04@users.noreply.github.com>
2019-10-04 13:41:06 +0530

    Add files via upload
    InfoGate consists of working useful tools that comes under category of EthicalHacking

--- InfoGate.py
+++ InfoGate.py
@@ -21,7 +21,9 @@ def main():
 		print "    [02] Red Hawk"
 		print "    [03] D-Tect"
 		print "    [04] sqlmap"
-		print "    [05] Zenmap\n"
+		print "    [05] Infoga"
+		print "    [06] ReconDog"
+		print "    [07] Zenmap\n"
 		print "    [00] Back to main menu\n"
 		infogathering = raw_input("GATE > ")
 		
@@ -34,6 +36,10 @@ def main():
 		elif infogathering == "04" or infogathering == "4":
 			sqlmap()
 		elif infogathering == "05" or infogathering == "5":
+			infoga()
+		elif infogathering == "06" or infogathering == "6":
+			reconDog()
+		elif infogathering == "07" or infogathering == "7":
 			Zenmap()
 		elif infogathering == "00" or infogathering == "0":
 			restart_program()
@@ -42,6 +48,61 @@ def main():
                         print "\nERROR: Wrong Input"
                         timeout(2)
                         restart_program()
+
+	elif InfoGate == "2" or InfoGate == "02":
+		print "\n    [01] Nmap"
+		print "    [02] AndroZenmap"
+		print "    [03] SQLiv"
+		print "    [04] sqlmap"
+		print "    [05] sqlscan"
+		print "    [06] Wordpresscan"
+		print "    [07] WPScan"
+		print "    [08] Striker"
+		print "    [09] Routersploit"
+		print "    [10] Xshell"
+		print "    [11] SH33LL"
+		print "    [12] BlackBox"
+		print "    [13] XAttacker"
+		print "    [14] OWScan\n"
+		print "    [00] Back to main menu\n"
+		scanning = raw_input("GATE > ")
+
+		if scanning == "01" or scanning == "1":
+			nmap()
+		elif scanning == "02" or scanning == "2":
+			androZenmap()
+		elif scanning == "03" or scanning == "3":
+			sqliv()
+		elif scanning == "04" or scanning == "4":
+			sqlmap()
+		elif scanning == "05" or scanning == "5":
+			sqlscan()
+		elif scanning == "06" or scanning == "6":
+			wordpreSScan()
+		elif scanning == "07" or scanning == "7":
+			wpscan()
+		elif scanning == "08" or scanning == "8":
+			striker()
+		elif scanning == "09" or scanning == "9":
+			routersploit()
+		elif scanning == "10":
+			xshell()
+		elif scanning == "11":
+			sh33ll()
+		elif scanning == "12":
+			blackbox()
+		elif scanning == "13":
+			xattacker()
+		elif scanning == "14":
+			owscan()
+		elif scanning == "00" or scanning == "0":
+			restart_program()
+		else:
+			print
+			"\nERROR: Wrong Input"
+			timeout(2)
+			restart_program()
+
 
 if __name__ == "__main__":
 	main()
--- infgat.py
+++ infgat.py
@@ -25,7 +25,7 @@ def restart_program():
 def backtomenu_option():
 	print backtomenu_banner
 	backtomenu = raw_input("GATE > ")
-	
+
 	if backtomenu == "99":
 		restart_program()
 	elif backtomenu == "00":
@@ -80,4 +80,137 @@ def Zenmap():
 	os.system('sudo apt-get install zenmap')
 	print '###### Done'
 	backtomenu_option()
-	
+
+def infoga():
+	print '\n###### Installing Infoga'
+	os.system('apt update && apt upgrade')
+	os.system('apt install python2 git')
+	os.system('python2 -m pip install requests urllib3 urlparse')
+	os.system('git clone https://github.com/m4ll0k/Infoga')
+	os.system('mv Infoga ~')
+	print '###### Done'
+	backtomenu_option()
+
+def reconDog():
+	print '\n###### Installing ReconDog'
+	os.system('apt update && apt upgrade')
+	os.system('apt install python2 git')
+	os.system('git clone https://github.com/UltimateHackers/ReconDog')
+	os.system('mv ReconDog ~')
+	print '###### Done'
+	backtomenu_option()
+
+def androZenmap():
+	print '\n###### Installing AndroZenmap'
+	os.system('apt update && apt upgrade')
+	os.system('apt install nmap curl')
+	os.system('curl -O https://raw.githubusercontent.com/Gameye98/Gameye98.github.io/master/scripts/androzenmap.sh')
+	os.system('mkdir ~/AndroZenmap')
+	os.system('mv androzenmap.sh ~/AndroZenmap')
+	print '###### Done'
+	backtomenu_option()
+
+def sqliv():
+	print '\n###### Installing SQLiv'
+	os.system('apt update && apt upgrade')
+	os.system('apt install python2 git')
+	os.system('git clone https://github.com/Hadesy2k/sqliv')
+	os.system('mv sqliv ~')
+	print '###### Done'
+	backtomenu_option()
+
+def sqlscan():
+	print '\n###### Installing sqlscan'
+	os.system('apt update && apt upgrade')
+	os.system('apt install git php')
+	os.system('git clone http://www.github.com/Cvar1984/sqlscan')
+	os.system('mv sqlscan ~')
+	print '###### Done'
+	backtomenu_option()
+
+def wordpreSScan():
+	print '\n###### Installing Wordpresscan'
+	os.system('apt update && apt upgrade')
+	os.system('apt install python2 python2-dev clang libxml2-dev libxml2-utils libxslt-dev')
+	os.system('git clone https://github.com/swisskyrepo/Wordpresscan')
+	os.system('mv Wordpresscan ~')
+	os.system('cd ~/Wordpresscan && python2 -m pip install -r requirements.txt')
+	print '###### Done'
+	backtomenu_option()
+
+def wpscan():
+	print '\n###### Installing WPScan'
+	os.system('apt update && apt upgrade')
+	os.system('apt install git ruby curl')
+	os.system('git clone https://github.com/wpscanteam/wpscan')
+	os.system('mv wpscan ~ && cd ~/wpscan')
+	os.system('gem install bundle && bundle config build.nokogiri --use-system-libraries && bundle install && ruby wpscan.rb --update')
+	print '###### Done'
+	backtomenu_option()
+
+def striker():
+	print '\n###### Installing Striker'
+	os.system('apt update && apt upgrade')
+	os.system('apt install git python2')
+	os.system('git clone https://github.com/UltimateHackers/Striker')
+	os.system('mv Striker ~')
+	os.system('cd ~/Striker && python2 -m pip install -r requirements.txt')
+	print '###### Done'
+	backtomenu_option()
+
+def routersploit():
+	print '\n###### Installing Routersploit'
+	os.system('apt update && apt upgrade')
+	os.system('apt install python2 git')
+	os.system('python2 -m pip install requests')
+	os.system('git clone https://github.com/reverse-shell/routersploit')
+	os.system('mv routersploit ~;cd ~/routersploit;python2 -m pip install -r requirements.txt;termux-fix-shebang rsf.py')
+	print '###### Done'
+	backtomenu_option()
+
+def xshell():
+	print '\n###### Installing Xshell'
+	os.system("apt update && apt upgrade")
+	os.system("apt install lynx python2 figlet ruby php nano w3m")
+	os.system("git clone https://github.com/Ubaii/Xshell")
+	os.system("mv Xshell ~")
+	print '###### Done'
+	backtomenu_option()
+
+def sh33ll():
+	print '\n###### Installing SH33LL'
+	os.system("apt update && apt upgrade")
+	os.system("apt install git python2")
+	os.system("git clone https://github.com/LOoLzeC/SH33LL")
+	os.system("mv SH33LL ~")
+	print '###### Done'
+	backtomenu_option()
+
+def blackbox():
+	print '\n###### Installing BlackBox'
+	os.system('apt update && apt upgrade')
+	os.system('apt install python2 git && python2 -m pip install optparse passlib')
+	os.system('git clone https://github.com/jothatron/blackbox')
+	os.system('mv blackbox ~')
+	print '###### Done'
+	backtomenu_option()
+
+def xattacker():
+	print '\n###### Installing XAttacker'
+	os.system('apt update && apt upgrade')
+	os.system('apt install git perl')
+	os.system('cpnm install HTTP::Request')
+	os.system('cpnm install LWP::Useragent')
+	os.system('git clone https://github.com/Moham3dRiahi/XAttacker')
+	os.system('mv XAttacker ~')
+	print '###### Done'
+	backtomenu_option()
+
+def owscan():
+	print '\n###### Installing OWScan'
+	os.system('apt update && apt upgrade')
+	os.system('apt install git php')
+	os.system('git clone https://github.com/Gameye98/OWScan')
+	os.system('mv OWScan ~')
+	print '###### Done'
+	backtomenu_option()
