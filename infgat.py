578bbac4ecd23ef22d3992986d6fbbe5c91c5552
Dark_Shadow04 <32217440+DarkShadow04@users.noreply.github.com>
2019-10-06 21:08:01 +0530

    Update infgat.py

--- infgat.py
+++ infgat.py
@@ -71,9 +71,9 @@ def red_hawk():
 	print '\n###### Installing RED HAWK'
 	os.system('apt update && apt upgrade')
 	os.system('apt install git php')
+	os.system('mkdir RED_HAWK && cd RED_HAWK')
 	os.system('git clone https://github.com/Tuhinshubhra/RED_HAWK')
-	os.system('mv -v RED_HAWK RED_HAWK')
-	os.system('mv -v RED_HAWK Scanning')
+	os.system('cd .. && mv -v RED_HAWK Scanning')
 	print '###### Done'
 	backtomenu_option()
 
